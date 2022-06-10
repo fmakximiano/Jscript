fb4c201664361d0bb462d8f74f8bdfeea41b9aee
fmakximiano <fernasbae@gmail.com>
2022-06-10 19:40:39 -0300

    segundo commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var nome = "fernanda"
+var numero = 26
+var ano = 2022
+
+console.log(ano - numero)
+console.log(ano + numero)
+console.log("Estou mexendo no console.log")
+let         sextaFeira = false
+console.log (quartaFeira)
+
+console.log(typeof quartaFeira)
+console.log(nome)
